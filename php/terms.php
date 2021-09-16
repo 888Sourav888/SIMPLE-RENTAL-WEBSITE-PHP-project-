569aebf9aa407b180d4b73d4131045e655d95ff7
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2021-09-16 16:42:48 +0530

    Update terms.php

--- php/terms.php
+++ php/terms.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name = 'author' content = "SOURAV KUMAR NRS 2020115089"> 
+    <meta name = 'author' content = ""> 
     <title>RentHeaven - Terms&Condition </title>
     <link rel = 'stylesheet' href= '../css/header.css?v=<?php echo time(); ?>'> 
     <link rel = 'stylesheet' href = '../css/terms.css?v=<?php echo time();?>'>
