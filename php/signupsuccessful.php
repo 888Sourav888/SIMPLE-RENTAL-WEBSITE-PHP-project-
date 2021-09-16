3ba4875c80c8e4e93cf65a131381f9772c01f056
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2021-09-16 16:43:22 +0530

    Update signupsuccessful.php

--- php/signupsuccessful.php
+++ php/signupsuccessful.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name = 'author' content = "SOURAV KUMAR NRS 2020115089"> 
+    <meta name = 'author' content = ""> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registered</title>
     <link rel = 'icon' href = '../images/logo.png'> 
