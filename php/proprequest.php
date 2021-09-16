46e284b16570743b63feb6d78aeaf3571668cc3b
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2021-09-16 16:43:45 +0530

    Update proprequest.php

--- php/proprequest.php
+++ php/proprequest.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name = 'author' content = "SOURAV KUMAR NRS 2020115089"> 
+    <meta name = 'author' content = ""> 
     <title>Rent Heaven - Requested</title>
     <link rel = 'icon' href = '../images/logo.png'> 
     <link rel = 'stylesheet' href = '../css/header.css?v=<?php echo time();?>'> 
